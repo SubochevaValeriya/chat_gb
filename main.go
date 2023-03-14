7c24223d4d5f7d199624e36cb858c78f539c24db
Valeriya Subocheva <96348645+SubochevaValeriya@users.noreply.github.com>
2023-03-14 23:05:09 +0300

    Added "Hello World" server

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
-func main(){
-	fmt.Println("Merge Conflict")
+func main() {
+	http.HandleFunc("/", helloWorld)
+	http.ListenAndServe(":8080", nil)
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
 }
